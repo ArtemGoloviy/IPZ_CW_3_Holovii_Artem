e1bc37c7e4f1ad122c6c5eac19730717b21aad83
ArtemGoloviy <58403271+ArtemGoloviy@users.noreply.github.com>
2024-03-13 15:50:25 +0200

    створюємо задній фон

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -1,21 +1,33 @@
-package ua.edu.lntu_cw_3
+package ua.edu.lntu.cw_3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw_3.ui.theme.IPZ_CW_3_Holovii_ArtemTheme
 
@@ -25,6 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IPZ_CW_3_Holovii_ArtemTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MainScreen(modifier = Modifier.padding(innerPadding))
                 }
@@ -35,6 +48,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+
+    val items = (1..31).toList().map { Item(it) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -42,15 +58,47 @@ fun MainScreen(modifier: Modifier = Modifier) {
     ) {
         Text(
             text = "30 day of sport",
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
-            style = TextStyle(fontSize = 30.sp, color = Color.Black)
+            style = TextStyle(fontSize = 32.sp, color = Color.Black)
         )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(items) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = colorResource(id = R.color.blue),
+                    ),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = "Day ${it.day}")
+                        Text(text = it.title, style = TextStyle(fontSize = 24.sp))
+                        Image(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp),
+                            painter = painterResource(id = it.randomImageRes),
+                            contentDescription = ""
+                        )
+                        Text(text = it.description)
+                    }
+                }
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    IPZ_CW_3_Holovii_ArtemTheme { MainScreen() }
+    IPZ_CW_3_Holovii_ArtemTheme {
+         MainScreen() }
 }
